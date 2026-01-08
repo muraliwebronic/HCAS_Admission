72adc0b013e2d94992e6888ac45db9ef9a317329
muraliwebronic <muralikrishna.webronic@gmail.com>
2026-01-08 19:34:11 +0530

    export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    output: "export",
   reactCompiler: true,
+  trailingSlash: true,
+  // ADD THIS SECTION
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
